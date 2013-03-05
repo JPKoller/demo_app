21c5088b0aea1e764c6911132a5642457d891d8e
Jonathan Koller <jkoller@bimlocal.com>
2013-03-05 14:19:08 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'f4c8f71bfe5725a2b5afc7ca432e635f9c6d7b9287c5b1bf908005c59862201c2ece799d662c612d6ca326f251d6842ec066290ef811418871b80e537beb9466'
